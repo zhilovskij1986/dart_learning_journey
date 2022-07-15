d508bb4a27cc8b362877bb2961e4e160bc749a94
Vitaliy <zhilovskij1986@gmail.com>
2022-07-15 04:56:06 +0300

    second function

--- lib/esson_04/homework.dart
+++ lib/esson_04/homework.dart
@@ -1,5 +1,6 @@
 void main() {
   primitives();  
+  collections();
 }
 
 /// Створи змінні та виведи їх:
@@ -20,6 +21,25 @@ void primitives() {
   }
 
   print('Мене звати $name , мені $age років, мій зріст $height, і я $myStatus');
+  print('______________________________________');
+}
+
+/// Завдання 2: Колекції
+/// 1. List — 3 твої улюблені кольори
+/// 2. Set — 3 унікальні оцінки, спробуй створити перевір print-ми, що вони не виводяться
+/// 3. Map — 3 предмети та їх оцінки
+void collections() {
+  List<String> favoriteColors = ['blue', 'white', 'orange'];
+  Set<int> grades = {10, 11, 12, 10, 11};
+  Map<String, int> gradesSubjects = {
+    'Mathematics': 10,
+    'English': 11,
+    'Computer_science': 12,
+  };
+
+  print('улюблені кольори: ${favoriteColors.join(", ")}');
+  print('унікальні оцінки ${grades.join(", ")}');
+  print('Предмети: ${gradesSubjects.keys} оцінки: ${gradesSubjects.values}');
   print('______________________________________');
 }
 
