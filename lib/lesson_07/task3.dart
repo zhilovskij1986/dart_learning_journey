f34b0aeed4f95a241cea3091da8f20772bc34213
Vitaliy <zhilovskij1986@gmail.com>
2026-03-28 17:15:06 +0200

    task3

---
+++ lib/lesson_07/task3.dart
@@ -0,0 +1,21 @@
+import 'package:mocky/mocky.dart';
+
+void main() {
+  final randomNouns = Mocky.words(100);
+
+  final nounsMap = <String, int>{};
+
+  for (final word in randomNouns) {
+    nounsMap[word] = word.length;
+  }
+ // print(nounsMap);
+
+  final nounsMapFiltered = <String, int>{};
+
+  for (final evenLength in nounsMap.entries) {
+    if (evenLength.value % 2 == 0) {
+      nounsMapFiltered[evenLength.key] = evenLength.value;
+    }
+  }
+  print(nounsMapFiltered.keys);
+}
