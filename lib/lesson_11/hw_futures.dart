6a8a486f8d006d47022eb733ca13d839da6ca09f
Vitaliy <zhilovskij1986@gmail.com>
2026-04-05 00:02:21 +0300

    task 2

--- lib/lesson_11/hw_futures.dart
+++ lib/lesson_11/hw_futures.dart
@@ -1,10 +1,44 @@
-void main() async {
-  final result = await fetchName();
+import 'dart:async';
 
-  print('Мене звати $result');
+void main() async {
+  task_1();
+  task_2();
+}
+
+void task_1() async {
+  final resultName = await fetchName();
+
+  print('Мене звати $resultName');
 }
 
 Future<String> fetchName() async {
   await Future<void>.delayed(Duration(seconds: 2));
   return 'Віталій';
+}
+
+void task_2() async {
+  final resulAge = await fetchAge();
+  if (resulAge == '') {
+    print('Не заповнено поле вік');
+  } else {
+    final age = int.parse(resulAge);
+    print('Мені $age ${getAgeAddition(age)}');
+  }
+}
+
+Future<String> fetchAge() async {
+  await Future<void>.delayed(Duration(milliseconds: 1500));
+  return '10';
+}
+
+String getAgeAddition(int age) {
+  if (age % 100 >= 11 && age % 100 <= 14) {
+    return 'років';
+  } else if (age % 10 == 1) {
+    return 'рік';
+  } else if (age % 10 >= 2 && age % 10 <= 4) {
+    return 'роки';
+  } else {
+    return 'років';
+  }
 }
