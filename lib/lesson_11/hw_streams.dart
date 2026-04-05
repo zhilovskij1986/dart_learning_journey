36fec0ae120323ab8169f56a9a6a57f5d287ee0f
Vitaliy <zhilovskij1986@gmail.com>
2026-04-05 11:04:29 +0300

    task 7

--- lib/lesson_11/hw_streams.dart
+++ lib/lesson_11/hw_streams.dart
@@ -1,4 +1,10 @@
 void main() async {
+  streamListen();
+
+  streamPeriodic();
+}
+
+void streamListen() async {
   final fromIterable = Stream<int>.fromIterable([1, 2, 3, 4, 5]);
 
   await for (final value in fromIterable) {
@@ -6,4 +12,12 @@ void main() async {
   }
   print('_______________________________');
   fromIterable.listen(print);
+}
+
+void streamPeriodic() {
+  Stream<int>.periodic(const Duration(seconds: 1), (i) => i + 1)
+      .take(10)
+      .listen((number) {
+        print('$number...');
+      });
 }
